51e9ac07b8f4a21bb7f4e09463fb9ae602b1ee80
Derrick <kang.derrick@gmail.com>
2020-04-05 18:16:21 +0900

    added a layout for app

---
+++ CoreDataSample/CoreDataSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by derrick on 2020/04/05.
+//  Copyright © 2020 derrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
